28f273800620d87e4aa80896141f8bead8c54c17
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-07-20 16:51:51 -0300

    [REFAC]: Melhorando display da data

--- Python/Basico/03.py
+++ Python/Basico/03.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
-# TODO: formatar o tempo, tirar a fração dos segundos
-print(f'A data e hora atual é: {datetime.now()}')
+date = datetime.now()
+print(f'A data e hora atual é: {date.strftime("%m/%d/%Y, %H:%M:%S")}')
 
