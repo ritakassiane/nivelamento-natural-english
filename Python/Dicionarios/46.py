f31c2bdf51b21e9ddff0122697a6a1595498e554
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-07-21 11:05:39 -0300

    [ADD]: Dado (key, value), retorna um {key: [values]}

---
+++ Python/Dicionarios/46.py
@@ -0,0 +1,8 @@
+
+def group_dict(list_):
+    result = {}
+    for key, value in list_:
+        result.setdefault(key, []).append(value)
+    return result
+    
+print(group_dict([('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]))
