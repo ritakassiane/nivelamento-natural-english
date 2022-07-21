71e81796993d7230c0b111a065a039901507e542
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-07-20 23:42:04 -0300

    [ADD]: Concatenação de dois dicionários, e soma dos valores das chaves em comum

---
+++ Python/Dicionarios/19.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+def concatenate_sum_values(dict1, dict2):
+    return Counter(dict1) + Counter(dict2)
+
+d1 = {'a': 100, 'b': 200, 'c':300}
+d2 = {'a': 300, 'b': 200, 'd':400}
+
+print(concatenate_sum_values(d1,d2))
