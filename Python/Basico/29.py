d38c7be5588735848581be217b700df2f9073547
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-07-20 23:31:58 -0300

    [REFACT]: Criando um objeto set com método difference()

--- Python/Basico/29.py
+++ Python/Basico/29.py
@@ -1,9 +1,10 @@
 color_list_1 = set(["White", "Black", "Red"])
 color_list_2 = set(["Red", "Green"])
 
-newObject = set()
+newObject = color_list_1.difference(color_list_2)
 
-for item in color_list_1:
-    if item not in color_list_2:
-        newObject.add(item)
+# for item in color_list_1:
+#     if item not in color_list_2:
+#         newObject.add(item)
+
 print(newObject)
