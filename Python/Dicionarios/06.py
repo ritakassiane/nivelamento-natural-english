9eb2a5753b97167f3dbcc014a9f2181e636dfc20
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-07-21 09:27:19 -0300

    [REFACt]: Utilizando dict comprehension para criar dicionarios no padrão elemento: elemento²

--- Python/Dicionarios/06.py
+++ Python/Dicionarios/06.py
@@ -1,10 +1,14 @@
-# list comprehension python
+# def dict_n_elements_square(n):
+#     dict = {}
+#     for element in range (1,n+1):
+#         dict[element]=element**2
+#     return dict
+
 
 def dict_n_elements_square(n):
-    dict = {}
-    for element in range (1,n+1):
-        dict[element]=element**2
+    dict = {elemento: elemento*2 for elemento in range(1,n+1)}
     return dict
+
 
 print(dict_n_elements_square((15)))
 
