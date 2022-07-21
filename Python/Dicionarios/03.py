f9d66e88b7d767a12abd563568d7a007141c06f5
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-07-20 23:37:46 -0300

    [ADD]: Funcionalidade para substituir caracteres de uma string por *

---
+++ Python/Dicionarios/03.py
@@ -0,0 +1,14 @@
+
+# list comprehension python
+
+def concatenate_dict(*dicts):
+    dict_result = {}
+    for dict in dicts:
+        dict_result.update(dict)
+    return dict_result
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+
+print(concatenate_dict(dic1, dic2, dic3))
